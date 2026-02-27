a28e368621148efec37076e2d5587e429a825563
Manoj Gowda B P <manojgowdabp43@gmail.com>
2026-02-27 14:39:58 +0530

    changes

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -91,7 +91,7 @@ export default function Footer() {
               </h3>
               <ul className="space-y-4 text-sm text-white/50 font-medium">
                 <li>
-                  <Link href="/about" className="hover:text-cyan-400 hover:translate-x-1 transition-all inline-block">
+                  <Link href="#/about" className="hover:text-cyan-400 hover:translate-x-1 transition-all inline-block">
                     About Us
                   </Link>
                 </li>
